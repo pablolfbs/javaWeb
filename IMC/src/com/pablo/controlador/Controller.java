34aca20b18808bfe454afde405a94532c8fb2762
pablolfbs <pablo.fariabritto@gmail.com>
2020-06-11 15:34:07 -0300

    criação do projeto imc

---
+++ IMC/src/com/pablo/controlador/Controller.java
@@ -0,0 +1,98 @@
+package com.pablo.controlador;
+
+import java.text.DecimalFormat;
+
+import javax.swing.JOptionPane;
+
+public class Controller {
+	
+	public void calculaIMC() {
+		try {
+			String strPeso = null;
+			String strAltura = null;
+
+			strPeso = formDados(strPeso, "seu peso");
+			float peso = Float.parseFloat(strPeso);
+
+			strAltura = formDados(strAltura, "sua altura");
+			float altura = Float.parseFloat(strAltura);
+
+			if (altura > 2)
+				altura = altura / 100;
+
+			float imc = peso / (altura * altura);
+
+			String msg = msgIMC(imc);
+
+			msgNulo(imc, msg);
+			msgAgradecimento();
+
+		} catch (NullPointerException e) {
+			msgAgradecimento();
+		}
+	}
+
+	private static void msgAgradecimento() {
+		JOptionPane.showMessageDialog(null, "Volte Sempre!.", "OBRIGADO", JOptionPane.PLAIN_MESSAGE);
+	}
+
+	private static void msgNulo(float imc, String msg) {
+		JOptionPane.showMessageDialog(null, "Seu IMC é " + new DecimalFormat("#.00").format(imc) + ". " + msg + ".");
+	}
+
+	private static String msgIMC(float imc) {
+		String msg = null;
+		if (imc < 16)
+			msg = "Baixo peso muito grave";
+		else if (imc < 17)
+			msg = "Baixo peso grave";
+		else if (imc < 18.5)
+			msg = "Baixo peso";
+		else if (imc < 25)
+			msg = "Peso normal";
+		else if (imc < 30)
+			msg = "Sobrepeso";
+		else if (imc < 35)
+			msg = "Obesidade grau I";
+		else if (imc < 40)
+			msg = "Obesidade grau II";
+		else
+			msg = "Obesidade grau III (obesidade mórbida)";
+		return msg;
+	}
+
+	private static String formDados(String valor, String param) {
+		int count = 0;
+		do {
+			if (count == 0)
+				valor = JOptionPane.showInputDialog(null, "Digite " + param + ": ", "IMC", JOptionPane.QUESTION_MESSAGE)
+						.replace(",", ".");
+			else
+				valor = JOptionPane.showInputDialog(null,
+						"Vamos tentar novamente." + System.lineSeparator() + "Digite " + param + ": ", "IMC",
+						JOptionPane.QUESTION_MESSAGE).replace(",", ".");
+
+			if (valor == null)
+				throw new NullPointerException();
+
+			verificaNulo(valor);
+
+			validaValor(valor);
+
+			count++;
+		} while (!valor.matches("^-?(?:\\d+|\\d{1,3}(?:,\\d{3})+)(?:(\\.|,)\\d+)?$"));
+		return valor;
+	}
+
+	private static void verificaNulo(String valor) {
+		if (valor.equals(""))
+			JOptionPane.showMessageDialog(null, "Não pode estar vazio.", "ERRO", JOptionPane.ERROR_MESSAGE);
+	}
+
+	private static void validaValor(String valor) {
+		if (!valor.matches("^-?(?:\\d+|\\d{1,3}(?:,\\d{3})+)(?:(\\.|,)\\d+)?$") && !valor.equals(""))
+			JOptionPane.showMessageDialog(null, "Não corresponde a um número válido.", "ERRO",
+					JOptionPane.ERROR_MESSAGE);
+	}
+
+}
