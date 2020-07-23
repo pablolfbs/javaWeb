79ae6761da0bb00f71db8c94ed7206da05fc28cd
pablolfbs <pablo.fariabritto@gmail.com>
2020-07-23 01:12:18 -0300

    updates

--- Hotel/src/controller/Ctrl.java
+++ Hotel/src/controller/Ctrl.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
