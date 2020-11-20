c579abe4a4b35ab9ed9e2e6a511d085c682848b8
pablolfbs <pablo.fariabritto@gmail.com>
2020-11-19 23:40:55 -0300

    updates

--- cjweb1/src/br/com/hightechcursos/jdbc/Conexao.java
+++ cjweb1/src/br/com/hightechcursos/jdbc/Conexao.java
@@ -12,8 +12,10 @@ public class Conexao {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost/cjweb1", "root", "");
 			System.out.println("Conectado com sucesso!!");
+			
 		} catch (SQLException e) {
 			System.out.println("Não pode conectar: " + e.getMessage());
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
