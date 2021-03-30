14b47efab73cccf87a04f3648f2959c5efca042e
pablolfbs <pablo.fariabritto@gmail.com>
2021-03-30 12:03:34 -0300

    utilização de lombok

--- Hotel/src/model/Usuario.java
+++ Hotel/src/model/Usuario.java
@@ -2,6 +2,15 @@ package model;
 
 import java.io.Serializable;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = -6093944336957320306L;
@@ -10,39 +19,12 @@ public class Usuario implements Serializable {
 	private String email;
 	private String senha;
 
-	public Usuario() {
-	}
-	
 	public Usuario(String email) {
 		this.email = email;
 	}
 
 	public Usuario(String email, String senha) {
 		this.email = email;
-		this.senha = senha;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
